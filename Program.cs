da0f1dca4c9fa557234a9b40847d203ec2f7cab5
mosalah0808 <mosalah0808@gmail.com>
2023-05-30 11:19:37 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Cleverence.Task3;
+
+int readerCount = 4;
+int writerCount = 2;
+
+// Запустим несколько потоков-читателей
+for (int i = 0; i < readerCount; i++)
+{
+    new Thread(() =>
+    {
+        while (true)
+        {
+            int count = Server.GetCount();
+            Console.WriteLine($"Reader {Thread.CurrentThread.ManagedThreadId} read count: {count}");
+            Thread.Sleep(1000);
+        }
+    }).Start();
+}
+
+// Запустим несколько потоков-писателей
+for (int i = 0; i < writerCount; i++)
+{
+    new Thread(() =>
+    {
+        while (true)
+        {
+            int value = new Random().Next(1, 3);
+            Server.AddToCount(value);
+            Console.WriteLine($"Writer {Thread.CurrentThread.ManagedThreadId} added {value} to count");
+            Thread.Sleep(2000);
+        }
+    }).Start();
+}
+
+Console.ReadLine();
---
+++ Server.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cleverence.Task3
+{
+    public static class Server
+    {
+        private static int count = 0;
+        private static readonly SemaphoreSlim readSemaphore = new SemaphoreSlim(1);
+        private static readonly SemaphoreSlim writeSemaphore = new SemaphoreSlim(1);
+
+        public static int GetCount()
+        {
+            readSemaphore.Wait(); // ждем, пока не будет доступен семафор для чтения
+            int result = count;
+            readSemaphore.Release(); // освобождаем семафор для чтения
+            return result;
+        }
+
+        public static void AddToCount(int value)
+        {
+            writeSemaphore.Wait(); // ждем, пока не будет доступен семафор для записи
+
+            try
+            {
+                readSemaphore.Wait(); // ждем, пока все потоки, которые читают, завершат чтение
+
+                count += value;
+            }
+            finally
+            {
+                readSemaphore.Release(); // освобождаем семафор для чтения
+                writeSemaphore.Release(); // освобождаем семафор для записи
+            }
+        }
+    }
+}
